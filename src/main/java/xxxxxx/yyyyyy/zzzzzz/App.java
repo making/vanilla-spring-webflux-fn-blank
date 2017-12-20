1e43c2a59f7e53c9e47d75393b729f7e224af3f0
making <makingx@gmail.com>
2017-12-21 01:01:36 +0900

    Polish

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
@@ -6,10 +6,8 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.ipc.netty.http.server.HttpServer;
-import reactor.ipc.netty.tcp.BlockingNettyContext;
 
 import java.util.Optional;
-import java.util.function.Consumer;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -22,18 +20,14 @@ public class App {
         return route(GET("/"), req -> ok().body(Flux.just("Hello", " ", "World!"), String.class));
     }
 
-    static void bootstrap(int port, Consumer<BlockingNettyContext> onStart) {
-        HttpServer httpServer = HttpServer.create("0.0.0.0", port);
-        httpServer.startRouterAndAwait(routes -> routes.route(x -> true,
-                new ReactorHttpHandlerAdapter(toHttpHandler(routes()))), onStart);
-    }
-
     public static void main(String[] args) throws Exception {
         long begin = System.currentTimeMillis();
         int port = Optional.ofNullable(System.getenv("PORT")) //
                 .map(Integer::parseInt) //
                 .orElse(8080);
-        bootstrap(port, x -> {
+        HttpServer httpServer = HttpServer.create("0.0.0.0", port);
+        httpServer.startRouterAndAwait(routes -> routes.route(x -> true,
+                new ReactorHttpHandlerAdapter(toHttpHandler(App.routes()))), x -> {
             long elapsed = System.currentTimeMillis() - begin;
             LoggerFactory.getLogger(App.class).info("Started in {} seconds", elapsed / 1000.0);
         });
