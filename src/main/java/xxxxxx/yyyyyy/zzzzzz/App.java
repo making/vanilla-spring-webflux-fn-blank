0988c738998101b9dd14dfbbd71bb05d4f8b04b0
making <makingx@gmail.com>
2017-12-21 11:29:42 +0900

    Polish

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
@@ -1,8 +1,10 @@
 package xxxxxx.yyyyyy.zzzzzz;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.ipc.netty.http.server.HttpServer;
@@ -11,7 +13,6 @@ import java.util.Optional;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class App {
@@ -26,8 +27,10 @@ public class App {
                 .map(Integer::parseInt) //
                 .orElse(8080);
         HttpServer httpServer = HttpServer.create("0.0.0.0", port);
-        httpServer.startRouterAndAwait(routes -> routes.route(x -> true,
-                new ReactorHttpHandlerAdapter(toHttpHandler(App.routes()))), context -> {
+        httpServer.startRouterAndAwait(routes -> {
+            HttpHandler httpHandler = RouterFunctions.toHttpHandler(App.routes());
+            routes.route(x -> true, new ReactorHttpHandlerAdapter(httpHandler));
+        }, context -> {
             long elapsed = System.currentTimeMillis() - begin;
             LoggerFactory.getLogger(App.class).info("Started in {} seconds", elapsed / 1000.0);
         });
