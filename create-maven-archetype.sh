8721b3cbc1da471572c3ec13a290430ecae48754
Toshiaki Maki <tmaki@pivotal.io>
2019-04-22 03:31:05 +0900

    Initial GraalVM support

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -8,18 +8,21 @@ cp -r src pom.xml tmp
 pushd tmp
 TMP_DIR=`pwd`
 
-sed -i.bk -e "s|am\.ik\.archetype|xxxxxx.yyyyyy.zzzzzz|g" pom.xml
-sed -i.bk -e "s|vanilla-spring-webflux-fn-blank|projectName|g" pom.xml
-sed -i.bk -e "s|github\.com/making/projectName|github.com/making/vanilla-spring-webflux-fn-blank|g" pom.xml
+sed -i.bk "s|am\.ik\.archetype|xxxxxx.yyyyyy.zzzzzz|g" pom.xml
+sed -i.bk "s|vanilla-spring-webflux-fn-blank|projectName|g" pom.xml
+sed -i.bk "s|github\.com/making/projectName|github.com/making/vanilla-spring-webflux-fn-blank|g" pom.xml
+sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" src/main/resources/META-INF/native-image/am.ik.archetype/vanilla-spring-webflux-fn-blank/app.json
+mv src/main/resources/META-INF/native-image/am.ik.archetype/vanilla-spring-webflux-fn-blank src/main/resources/META-INF/native-image/am.ik.archetype/projectName
+mv src/main/resources/META-INF/native-image/am.ik.archetype src/main/resources/META-INF/native-image/xxxxxx.yyyyyy.zzzzzz
 
 mvn archetype:create-from-project
 
 pushd target/generated-sources/archetype
-sed -i.bk -e "/${KEYWORD}/d" target/classes/archetype-resources/pom.xml
-sed -i.bk -e "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" target/classes/archetype-resources/pom.xml
-sed -i.bk -e "s|xxxxxx\.yyyyyy\.zzzzzz|am.ik.archetype|g" pom.xml
-sed -i.bk -e "s|projectName|vanilla-spring-webflux-fn-blank|g" pom.xml
-sed -i.bk -e "s|<scm>|<distributionManagement><snapshotRepository><id>repo</id><url>https://oss.sonatype.org/content/repositories/snapshots</url></snapshotRepository><repository><id>repo</id><url>https://oss.sonatype.org/service/local/staging/deploy/maven2/</url></repository></distributionManagement><scm>|g" pom.xml
-sed -i.bk -e "s|<build>|<build><plugins><plugin><groupId>org.sonatype.plugins</groupId><artifactId>nexus-staging-maven-plugin</artifactId><version>1.6.8</version><extensions>true</extensions><configuration><serverId>repo</serverId><nexusUrl>https://oss.sonatype.org/</nexusUrl><autoReleaseAfterClose>true</autoReleaseAfterClose></configuration></plugin><plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-gpg-plugin</artifactId><version>1.6</version><executions><execution><id>sign-artifacts</id><phase>verify</phase><goals><goal>sign</goal></goals></execution></executions></plugin></plugins>|g" pom.xml
+sed -i.bk "/${KEYWORD}/d" target/classes/archetype-resources/pom.xml
+sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" target/classes/archetype-resources/pom.xml
+sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|am.ik.archetype|g" pom.xml
+sed -i.bk "s|projectName|vanilla-spring-webflux-fn-blank|g" pom.xml
+sed -i.bk "s|<scm>|<distributionManagement><snapshotRepository><id>repo</id><url>https://oss.sonatype.org/content/repositories/snapshots</url></snapshotRepository><repository><id>repo</id><url>https://oss.sonatype.org/service/local/staging/deploy/maven2/</url></repository></distributionManagement><scm>|g" pom.xml
+sed -i.bk "s|<build>|<build><plugins><plugin><groupId>org.sonatype.plugins</groupId><artifactId>nexus-staging-maven-plugin</artifactId><version>1.6.8</version><extensions>true</extensions><configuration><serverId>repo</serverId><nexusUrl>https://oss.sonatype.org/</nexusUrl><autoReleaseAfterClose>true</autoReleaseAfterClose></configuration></plugin><plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-gpg-plugin</artifactId><version>1.6</version><executions><execution><id>sign-artifacts</id><phase>verify</phase><goals><goal>sign</goal></goals></execution></executions></plugin></plugins>|g" pom.xml
 
 rm -f `find ${TMP_DIR} -name '*.bk'`
