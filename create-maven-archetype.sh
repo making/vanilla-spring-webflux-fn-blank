e7c35fd7cc359860713e8c32481045db21759a27
Toshiaki Maki <tmaki@pivotal.io>
2019-05-26 17:08:25 +0900

    Support maven-archetype-plugin:3.1.0

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -14,17 +14,17 @@ sed -i.bk "s|github\.com/making/projectName|github.com/making/vanilla-spring-web
 mv src/main/resources/META-INF/native-image/am.ik.archetype/vanilla-spring-webflux-fn-blank src/main/resources/META-INF/native-image/am.ik.archetype/projectName
 mv src/main/resources/META-INF/native-image/am.ik.archetype src/main/resources/META-INF/native-image/xxxxxx.yyyyyy.zzzzzz
 
-mvn org.apache.maven.plugins:maven-archetype-plugin:3.0.1:create-from-project
+mvn archetype:create-from-project
 
 pushd target/generated-sources/archetype
 sed -i.bk "/${KEYWORD}/d" target/classes/archetype-resources/pom.xml
-sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" target/classes/archetype-resources/pom.xml
+sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${packageName}|g" target/classes/archetype-resources/pom.xml
 sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|am.ik.archetype|g" pom.xml
 sed -i.bk "s|projectName|vanilla-spring-webflux-fn-blank|g" pom.xml
 sed -i.bk "s|<scm>|<distributionManagement><snapshotRepository><id>repo</id><url>https://oss.sonatype.org/content/repositories/snapshots</url></snapshotRepository><repository><id>repo</id><url>https://oss.sonatype.org/service/local/staging/deploy/maven2/</url></repository></distributionManagement><scm>|g" pom.xml
 sed -i.bk "s|<build>|<build><plugins><plugin><groupId>org.sonatype.plugins</groupId><artifactId>nexus-staging-maven-plugin</artifactId><version>1.6.8</version><extensions>true</extensions><configuration><serverId>repo</serverId><nexusUrl>https://oss.sonatype.org/</nexusUrl><autoReleaseAfterClose>true</autoReleaseAfterClose></configuration></plugin><plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-gpg-plugin</artifactId><version>1.6</version><executions><execution><id>sign-artifacts</id><phase>verify</phase><goals><goal>sign</goal></goals></execution></executions></plugin></plugins>|g" pom.xml
 sed -i.bk '/<include>\*\*\/\*.json<\/include>/d' ./src/main/resources/META-INF/maven/archetype-metadata.xml
-sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" src/main/resources/archetype-resources/src/main/resources/META-INF/native-image/__package__/__artifactId__/app.json
+sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${packageName}|g" src/main/resources/archetype-resources/src/main/resources/META-INF/native-image/__package__/__artifactId__/app.json
 
 sed -i.bk 's|<include>\*\*/\*.properties</include>|<include>**/*.properties</include><include>**/*.json</include>|' ./src/main/resources/META-INF/maven/archetype-metadata.xml
 rm -f `find ${TMP_DIR} -name '*.bk'`
