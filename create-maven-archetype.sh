ba0298fbc9eef2273bfd4a8f95b6975c861aa0a2
Toshiaki Maki <tmaki@pivotal.io>
2019-09-07 00:12:44 +0900

    Simplify configs for native-image

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -24,7 +24,7 @@ sed -i.bk "s|projectName|vanilla-spring-webflux-fn-blank|g" pom.xml
 sed -i.bk "s|<scm>|<distributionManagement><snapshotRepository><id>repo</id><url>https://oss.sonatype.org/content/repositories/snapshots</url></snapshotRepository><repository><id>repo</id><url>https://oss.sonatype.org/service/local/staging/deploy/maven2/</url></repository></distributionManagement><scm>|g" pom.xml
 sed -i.bk "s|<build>|<build><plugins><plugin><groupId>org.sonatype.plugins</groupId><artifactId>nexus-staging-maven-plugin</artifactId><version>1.6.8</version><extensions>true</extensions><configuration><serverId>repo</serverId><nexusUrl>https://oss.sonatype.org/</nexusUrl><autoReleaseAfterClose>true</autoReleaseAfterClose></configuration></plugin><plugin><groupId>org.apache.maven.plugins</groupId><artifactId>maven-gpg-plugin</artifactId><version>1.6</version><executions><execution><id>sign-artifacts</id><phase>verify</phase><goals><goal>sign</goal></goals></execution></executions></plugin></plugins>|g" pom.xml
 sed -i.bk '/<include>\*\*\/\*.json<\/include>/d' ./src/main/resources/META-INF/maven/archetype-metadata.xml
-sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" src/main/resources/archetype-resources/src/main/resources/META-INF/native-image/__package__/__artifactId__/app.json
+sed -i.bk "s|xxxxxx\.yyyyyy\.zzzzzz|\${package}|g" src/main/resources/archetype-resources/src/main/resources/META-INF/native-image/__package__/__artifactId__/reflect-config.json
 
 sed -i.bk 's|<include>\*\*/\*.properties</include>|<include>**/*.properties</include><include>**/*.json</include>|' ./src/main/resources/META-INF/maven/archetype-metadata.xml
 rm -f `find ${TMP_DIR} -name '*.bk'`
